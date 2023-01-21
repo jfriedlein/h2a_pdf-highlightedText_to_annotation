8910f9ece6075cfb770c66344a0c7e608aa488d5
jfriedlein <58786257+jfriedlein@users.noreply.github.com>
2023-01-21 15:35:18 +0100

    generalised file and added comments

--- h2a-GUI.bat
+++ h2a-GUI.bat
@@ -1,2 +1,4 @@
-cd C:\Users\Johan\Desktop\h2a_pdf-highlightedText_to_annotation-main
+# Change directory, if you place this batch file on the desktop for instance:
+cd <path to folder>\h2a_pdf-highlightedText_to_annotation-main
+# Execute the python h2a GUI
 python h2a_GUI.py
