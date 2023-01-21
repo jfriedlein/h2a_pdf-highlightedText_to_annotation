6fbe7cf89539d868b7541439db0c793b48dd9393
jfriedlein <58786257+jfriedlein@users.noreply.github.com>
2023-01-21 17:40:27 +0100

    updated path

--- h2a-GUI.bat
+++ h2a-GUI.bat
@@ -1,4 +1,5 @@
-# Change directory, if you place this batch file on the desktop for instance:
-cd <path to folder>\h2a_pdf-highlightedText_to_annotation-main
-# Execute the python h2a GUI
+REM Change directory, if you place this batch file on the desktop for instance:
+REM Also remember to use e.g. "D:" before "cd D:\..." to change from default "C:" to "D:" if needed
+cd "<path to folder>\h2a_pdf-highlightedText_to_annotation-main"
+REM Execute the python h2a GUI
 python h2a_GUI.py
