9b66d4a8a3962d6cf7e884f59896ce21c6dcdb02
jfriedlein <58786257+jfriedlein@users.noreply.github.com>
2023-01-21 17:49:54 +0100

    removed paths

--- h2a-GUI.bat
+++ h2a-GUI.bat
@@ -1,5 +1,2 @@
-REM Change directory, if you place this batch file on the desktop for instance:
-REM Also remember to use e.g. "D:" before "cd D:\..." to change from default "C:" to "D:" if needed
-cd "<path to folder>\h2a_pdf-highlightedText_to_annotation-main"
 REM Execute the python h2a GUI
 python h2a_GUI.py
