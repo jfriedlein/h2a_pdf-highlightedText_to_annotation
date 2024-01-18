662f545cc919c3993e48ad8001ece74a87445760
jfriedlein <johannes.friedlein@fau.de>
2024-01-18 12:02:08 +0100

    playing with action what to do when pdf_time misses a timezone information, yet unresolved and untested

--- h2a_functions/get_datetime_from_pdftime.py
+++ h2a_functions/get_datetime_from_pdftime.py
@@ -2,8 +2,11 @@ import datetime
 #import pytz
 def get_datetime_from_pdftime( pdf_time ):
     # juggle between different date formats
-    if ( "'" in pdf_time ):
+    if ( "'" in pdf_time ): # timezone information is given
         #return datetime.datetime.strptime(pdf_time.replace("'", ""), "D:%Y%m%d%H%M%S%z").astimezone(pytz.timezone('Europe/Berlin')).strftime("D:%Y%m%d%H%M%S%z")
         return datetime.datetime.strptime(pdf_time.replace("'", ""), "D:%Y%m%d%H%M%S%z").replace(tzinfo=None)
-    else:
-        return datetime.datetime.strptime(pdf_time, "D:%Y%m%d%H%M%S")#.astimezone(pytz.timezone('Europe/Berlin')).strftime("D:%Y%m%d%H%M%S%z")
+    else: # no timezone information is given
+        print( "get_datetime_from_pdftime<< Missing timezone in pdf_time="+pdf_time )
+        # This is only attempting a bugfix of older okular versions, this block should normally not be called
+        # @todo Currently we assume the local time zone is "+1", so we add one hour
+        return datetime.datetime.strptime(pdf_time, "D:%Y%m%d%H%M%S") + datetime.timedelta(hours=1)
