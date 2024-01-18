081179e3f74f6f832cbd2330b3dadb9d905491c7
jfriedlein <johannes.friedlein@fau.de>
2024-01-18 14:25:51 +0100

    added docu, formatting

--- h2a_functions/get_new_title.py
+++ h2a_functions/get_new_title.py
@@ -1,6 +1,7 @@
 def get_new_title ( annot, title_suffix ):
+    # Add the title_suffix to the title if it is not already there
     old_title = annot.info['title'] 
     if ( title_suffix not in old_title ):
-        return old_title+title_suffix
+        return old_title + title_suffix
     else:
         return old_title
