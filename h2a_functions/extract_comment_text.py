404bd483cbc3b9bc0f104f98852c796e0dab772c
jfriedlein <johannes.friedlein@fau.de>
2024-01-18 14:26:54 +0100

    updated to general use of autoMarker, risking direct use of modDate without using get_datetime (might now cause issues for older buggy okular where timezone information was missing)

--- h2a_functions/extract_comment_text.py
+++ h2a_functions/extract_comment_text.py
@@ -2,18 +2,21 @@
 import re
 from h2a_functions.get_datetime_from_pdftime import get_datetime_from_pdftime
 
-def extract_comment_text( annot, H2A_protocol, title_suffix ):
+def extract_comment_text( annot, H2A_protocol, title_suffix, autoMarker ):
+    # Extract the current content of this annotation
     annot_text = annot.info['content']
-    # if the comment text contains a marker (>a>|>p>), it is clear how to split the text into extracted text and comment text
-    if ( ('>a>' in annot_text) or ('>p>' in annot_text) ):
-        # Extract the text after ([-1]) the marker (>a>|>p>) and remove possible leading whitespace (lstrip())
-        return re.split(r'>a>|>p>',annot_text)[-1].lstrip()
-    # if not, the text is either a pure comment (without yet using h2a) or a pure highlight text by using h2a without marker
+    # If the comment text contains an autoMarker (e.g. ' >a> '), it is clear how to split the text into extracted text and original comment text
+    if ( autoMarker in annot_text ):
+        # Extract the text after ([-1]) the marker (>a>) and remove possible leading whitespace (lstrip())
+        return re.split(autoMarker,annot_text)[-1].lstrip()
+    # Else the text is either a pure comment (without yet using h2a, so an empty H2A_protocal)
     elif ( len(H2A_protocol)==0 ):
         return annot_text
+    # Or else it is a pure highlight text by using h2a without marker
     else:
         list_of_h2a_runs = re.split(r' |; ',H2A_protocol[2])[2:-1]
-        modDate = get_datetime_from_pdftime( annot.info['modDate'] ).strftime("D:%Y%m%d%H%M%S%z")
+        #modDate = get_datetime_from_pdftime( annot.info['modDate'] ).strftime("D:%Y%m%d%H%M%S%z")
+        modDate = annot.info['modDate']
         # Adding the title_suffix to the annotation is important to be able to
         # distinguish original comment text from manually modified extracted highlight text
         # @todo Rethink the following criteria, is this repetitive?
