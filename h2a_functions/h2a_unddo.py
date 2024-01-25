47f9e73828564479faddcdb467c27d8a6077a944
jfriedlein <johannes.friedlein@fau.de>
2024-01-25 17:36:05 +0100

    changed import of h2a_functions, added autoMarker for for new use of extract_comment_text function

--- h2a_functions/h2a_unddo.py
+++ h2a_functions/h2a_unddo.py
@@ -1,15 +1,14 @@
 import fitz
-# Load the h2a modules from the subfolder "h2a_functions"
-import sys
-sys.path.append('./h2a_functions')
-from extract_comment_text import extract_comment_text
-from retrieve_H2A_protocol import retrieve_H2A_protocol
+from h2a_functions.extract_comment_text import extract_comment_text
+from h2a_functions.retrieve_H2A_protocol import retrieve_H2A_protocol
 
 def h2a_unddo( input_file ):
     # Open the pdf into "doc"
     doc = fitz.open(input_file)
     
+    # This might have to be saved in the H2A-protocol, because the user can decide this and it could change for each PDF.
     title_suffix = '_h2a'
+    autoMarker = ' >a> '
     
     # Read H2A-protocol
     H2A_protocol = retrieve_H2A_protocol( doc[0] )
@@ -22,7 +21,7 @@ def h2a_unddo( input_file ):
                  title = annot.info['title']
                  if ( title_suffix in title ):
                      # Extract comment part (after '>*>') from annot_text
-                     comment_text = extract_comment_text( annot, H2A_protocol, title_suffix ) 
+                     comment_text = extract_comment_text( annot, H2A_protocol, title_suffix, autoMarker ) 
                      # replace content of annotation by original comment text and remove title suffix
                      annot.set_info( content=comment_text, title=title[0:len(title)-len(title_suffix)] )
              if ( i_page==0 and protocol_deleted==False ):
